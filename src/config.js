547626c8b7dc6e4d1581ca1c97f43412e9ebe823
Cooper Maruyama <cooperm@susteen.com>
2016-12-01 13:11:25 -0800

    update config.

--- src/config.js
+++ src/config.js
@@ -16,24 +16,17 @@ export const analytics = {
 
 export const auth = {
 
-  jwt: { secret: process.env.JWT_SECRET || 'React Starter Kit' },
+  jwt: { secret: process.env.JWT_SECRET || 's3cr3t' },
 
   // https://developers.facebook.com/
   facebook: {
-    id: process.env.FACEBOOK_APP_ID || '186244551745631',
-    secret: process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+    id: process.env.FACEBOOK_APP_ID || 'XXXXXXXXXXXXXXXX',
+    secret: process.env.FACEBOOK_APP_SECRET || 'XXXXXXXXXXXXXXXXX',
   },
 
   // https://cloud.google.com/console/project
   google: {
-    id: process.env.GOOGLE_CLIENT_ID || '251410730550-ahcg0ou5mgfhl8hlui1urru7jn5s12km.apps.googleusercontent.com',
-    secret: process.env.GOOGLE_CLIENT_SECRET || 'Y8yR9yZAhm9jQ8FKAL8QIEcd',
-  },
-
-  // https://apps.twitter.com/
-  twitter: {
-    key: process.env.TWITTER_CONSUMER_KEY || 'Ie20AZvLJI2lQD5Dsgxgjauns',
-    secret: process.env.TWITTER_CONSUMER_SECRET || 'KTZ6cxoKnEakQCeSpZlaUCJWGAlTEBJj0y2EMkUBujA7zWSvaQ',
-  },
-
+    id: process.env.GOOGLE_CLIENT_ID || 'XXXXXXXXXXXX',
+    secret: process.env.GOOGLE_CLIENT_SECRET || 'XXXXXXXXXXX',
+  }
 };
