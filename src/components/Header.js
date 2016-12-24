99d39fb9a22d447d8dfa7b73ba1dad2e47b7c0b1
Cooper Maruyama <cooperm@susteen.com>
2016-12-24 00:19:41 -0800

    feat(home/header): Mobile styles.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import reactCSS from 'reactcss';
+import bounds from 'react-bounds';
 import { Link } from 'react-router';
 import Logo from './Logo';
 import LanguageSelector from './LanguageSelector';
 
-const styles = reactCSS({
+const stylesheet = {
   'default': {
     container: {
       width: '100%',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'space-between',
-      padding: '20px 10px'
+      padding: '20px 15px'
     },
     logo: {
-      height: '40px'
+      height: '30px',
+      display: 'flex',
+      alignItems: 'center'
     },
     links: {
       display: 'flex',
@@ -28,34 +31,113 @@ const styles = reactCSS({
     },
     activeLink: {
 
+    },
+    hamburger: {
+      color: 'white',
+      fontSize: '28px',
+      cursor: 'pointer'
+    },
+    close: {
+      position: 'absolute',
+      right: '10px',
+      top: '10px',
+      cursor: 'pointer'
+    }
+  },
+  'mobile': {
+    logo: {
+      height: '40px'
+    },
+    links: {
+      position: 'absolute',
+      top: '0',
+      left: '0',
+      right: '0',
+      background: 'hsl(0, 0%, 15%)',
+      color: 'hsl(0, 0%, 100%)',
+      height: '100vh',
+      padding: '20px',
+      display: 'flex',
+      flexDirection: 'column',
+      textAlign: 'left',
+      alignItems: 'flex-start'
+    },
+    link: {
+      fontSize: '20px',
+      margin: '20px 0'
     }
   }
-});
+};
 
-const Header = () => (
-  <div style={styles.container}>
-    <div style={styles.logo}>
-      <Logo style={styles.logo} />
-    </div>
-    <div style={styles.links}>
-      <Link
-        to="/meetings"
-        style={styles.link}
-        activeStyle={styles.activeLink}
-      >Online Meetings</Link>
-      <Link
-        to="/tapes"
-        style={styles.link}
-        activeStyle={styles.activeLink}
-      >Speakertapes</Link>
-      <Link
-        to="service-opportunities"
-        style={styles.link}
-        activeStyle={styles.activeLink}
-      >Service Opportunities</Link>
-      <LanguageSelector />
-    </div>
-  </div>
-);
+class Header extends React.Component {
 
-export default Header;
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false
+    };
+  }
+
+  static bounds() {
+    return {
+      'mobile': {
+        maxWidth: 800
+      }
+    };
+  }
+
+  render() {
+    const isMobile = this.props.isBound('mobile');
+    const styles = reactCSS(stylesheet, {
+      'mobile': isMobile
+    });
+    const btnClose = (
+      <i
+        className="material-icons"
+        style={styles.close}
+        onClick={() => this.setState({ open: false })}
+      >close</i>
+    );
+    const nav = (
+      <div style={styles.links}>
+        <Link
+          to="/meetings"
+          style={styles.link}
+          activeStyle={styles.activeLink}
+        >Online Meetings</Link>
+        <Link
+          to="/tapes"
+          style={styles.link}
+          activeStyle={styles.activeLink}
+        >Speakertapes</Link>
+        <Link
+          to="service-opportunities"
+          style={styles.link}
+          activeStyle={styles.activeLink}
+        >Service Opportunities</Link>
+        <LanguageSelector />
+        {isMobile && btnClose}
+      </div>
+    );
+    const hamburger = (
+      <i
+        style={styles.hamburger}
+        className="material-icons"
+        onClick={() => this.setState({ open: true })}
+      >menu</i>
+    );
+
+    return (
+      <div style={styles.container}>
+        <div style={styles.logo}>
+          <Logo style={styles.logo} />
+        </div>
+        {!isMobile && nav}
+        {isMobile && hamburger}
+        {isMobile && this.state.open && nav}
+      </div>
+    );
+  }
+}
+
+export default bounds.wrap(Header, { height: 'auto' });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,88 @@
 import React from 'react';
 import reactCSS from 'reactcss';
+import bounds from 'react-bounds';
 import Header from './Header';
 
-const styles = reactCSS({
+const stylesheet = {
   'default': {
-    container: {
+    hero: {
       backgroundImage: 'url(/img/bg-hero.png)',
       backgroundSize: 'cover',
       height: '500px'
+    },
+    heroBody: {
+      textAlign: 'center',
+      marginTop: '70px',
+      color: 'white'
+    },
+    intro: {
+      textTransform: 'uppercase',
+      fontSize: '20px'
+    },
+    hr: {
+      width: '55px'
+    },
+    headline: {
+      marginTop: '35px',
+      fontSize: '65px',
+      fontWeight: 500
+    },
+    heroDescription: {
+      marginTop: '35px',
+      fontSize: '16px'
     }
+  },
+  'mobile': {
+    hero: {
+      height: '330px'
+    },
+    heroBody: {
+      marginTop: '15px'
+    },
+    intro: {
+      fontSize: '17px'
+    },
+    headline: {
+      marginTop: '15px',
+      fontSize: '44px'
+    },
+    heroDescription: {
+      marginTop: '12px',
+      fontSize: '15px'
+    }
+  }
+};
+
+const Home = (props) => {
+  const styles = reactCSS(stylesheet, {
+    mobile: props.isBound('mobile')
+  });
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.hero}>
+        <Header />
+        <div style={styles.heroBody}>
+          <div style={styles.intro}>
+            Good Morning!
+          </div>
+          <hr style={styles.hr} />
+          <div style={styles.headline}>
+            Recovery for the world
+          </div>
+          <div style={styles.heroDescription}>
+            Onlin Meetings & Speakertapes &bull; 7 fellowships &bull; 13 languages
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Home.bounds = () => ({
+  'mobile': {
+    maxWidth: 800
   }
 });
 
-const Home = () => (
-  <div style={styles.container}>
-    <div style={styles.hero}>
-      <Header />
-    </div>
-  </div>
-);
-
-export default Home;
+export default bounds.wrap(Home);
